39e577c07d6577c03a6a18eb25115f4be50275e5
Jeff Kala <jeff.kala@networktocode.com>
2022-02-15 16:16:43 -0600

    initial

---
+++ tests/unit/conftest.py
@@ -0,0 +1,9 @@
+"""Used to setup fixtures to be used through tests"""
+import pytest
+from click.testing import CliRunner
+
+
+@pytest.fixture
+def cli_runner():
+    """Provide CLI runner for Click tests."""
+    return CliRunner()
---
+++ tests/unit/test_cli.py
@@ -0,0 +1,14 @@
+"""Example Test using Fixtures."""
+import mock
+from nornir_pyntc import cli
+
+
+@mock.patch("nornir_pyntc.cli.log")
+def test_cli_logging(log, cli_runner):
+    """Assert our logging gets called in CLI app."""
+    result = cli_runner.invoke(cli.main, ["--test", "ntc"])
+
+    assert result.exit_code == 0
+    assert result.output == "ntc\n"
+    log.info.assert_called()
+    log.info.assert_called_with("Entrypoint of the CLI app.")
