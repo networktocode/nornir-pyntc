3764d9c7f9291bb92ba72c8aa8d024f7610c9e77
Jeff Kala <jeff.kala@networktocode.com>
2022-02-16 15:36:54 -0600

    update show to accept kwargs

--- nornir_pyntc/tasks/pyntc_show.py
+++ nornir_pyntc/tasks/pyntc_show.py
@@ -1,10 +1,12 @@
 """Send a non-configuration command."""
 
+from typing import Any
+
 from nornir.core.task import Result, Task
 from nornir_pyntc.connections import CONNECTION_NAME
 
 
-def pyntc_show(task: Task, command: str, raw_text: bool = False) -> Result:
+def pyntc_show(task: Task, command: str, **kwargs: Any) -> Result:
     """Send a non-configuration command.
 
     Args:
@@ -14,5 +16,5 @@ def pyntc_show(task: Task, command: str, raw_text: bool = False) -> Result:
         The output of the show command, which could be raw text or structured data.
     """
     pyntc_connection = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-    result = pyntc_connection.show(command, raw_text=raw_text)
+    result = pyntc_connection.show(command, **kwargs)
     return Result(host=task.host, result=result)
