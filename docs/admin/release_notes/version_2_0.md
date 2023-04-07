11170defdfcb4b85d016abb3af9420d9a3ec6280
Jeff Kala <jeff.kala@networktocode.com>
2023-04-07 12:02:36 -0600

    prep for 2.0.0 with 1.0.0 of pyntc being release

---
+++ docs/admin/release_notes/version_2_0.md
@@ -0,0 +1,16 @@
+# v2.0.0 Release Notes
+
+## [2.0.0] 04-2023
+
+### Deprecated
+
+- show_list tasks.
+- Timer arg within reboot.
+
+### Added
+
+- Add `wait_for_reload` kwarg in `pyntc_reboot` task to mirror updated pyntc methods.
+
+### Fixed
+
+- `pyntc_config` - Update docstrings and support both list|str inputs.
--- nornir_pyntc/tasks/__init__.py
+++ nornir_pyntc/tasks/__init__.py
@@ -6,11 +6,9 @@ from nornir_pyntc.tasks.pyntc_install_os import pyntc_install_os
 from nornir_pyntc.tasks.pyntc_reboot import pyntc_reboot
 from nornir_pyntc.tasks.pyntc_save import pyntc_save
 from nornir_pyntc.tasks.pyntc_show import pyntc_show
-from nornir_pyntc.tasks.pyntc_show_list import pyntc_show_list
 
 __all__ = (
     "pyntc_show",
-    "pyntc_show_list",
     "pyntc_config",
     "pyntc_file_copy",
     "pyntc_install_os",
